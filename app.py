404570fef6fb13f7897490add20720c8e469247d
Tanguy Albrici <tanguy.albrici@epfl.ch>
2017-02-20 20:26:07 +0100

    display 2 images side-by-side

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-import tkinter
-top = tkinter.Tk()
-top.mainloop()
+import tkinter as tk
+from PIL import ImageTk, Image
+
+root = tk.Tk()
+root.title("lf-tracking")
+
+img = ImageTk.PhotoImage(Image.open("img.jpg"))
+
+tk.Label(root, image = img).grid(column = 0, row = 0)
+tk.Label(root, image = img).grid(column = 1, row = 0)
+tk.Label(root, text="Image 1").grid(column = 0, row = 1)
+tk.Label(root, text="Image 2").grid(column = 1, row = 1)
+
+root.mainloop()
