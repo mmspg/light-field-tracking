2a90d3e476494a018e2e1f165d24d7a9c6884b53
Tanguy Albrici <tanguy.albrici@epfl.ch>
2017-02-22 19:34:39 +0100

    setup PyDev IDE

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ root.title("lf-tracking")
 
 img = ImageTk.PhotoImage(Image.open("img.jpg"))
 
-tk.Label(root, image = img).grid(column = 0, row = 0)
-tk.Label(root, image = img).grid(column = 1, row = 0)
-tk.Label(root, text="Image 1").grid(column = 0, row = 1)
-tk.Label(root, text="Image 2").grid(column = 1, row = 1)
+tk.Label(root, image=img).grid(column=0, row=0)
+tk.Label(root, image=img).grid(column=1, row=0)
+tk.Label(root, text="Image 1").grid(column=0, row=1)
+tk.Label(root, text="Image 2").grid(column=1, row=1)
 
 root.mainloop()
