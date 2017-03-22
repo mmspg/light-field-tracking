84df5c625d0ad75ff8830d70c3a7a24e10587386
Tanguy Albrici <tanguy.albrici@epfl.ch>
2017-03-23 00:49:21 +0100

    add 'Slideshow' class and refactoring

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import datetime
 
 BG_COLOR = "grey"
 IMG_PATH_PREFIX = "img/"
-IMG_NAMES = ["img0", "img1", "img2"]
 
 f = open('output.txt', 'w')
-
 
 
 class FullScreenApp(object):
@@ -24,12 +22,6 @@ class FullScreenApp(object):
         print(geom, self._geom)
         self.master.geometry(self._geom)
         self._geom = geom
-
-
-root = tk.Tk()
-root.tk.call('tk', 'scaling', 2.0)
-root.title("lf-tracking")
-root.configure(background=BG_COLOR)
 
 
 def clamp(x, minimum, maximum):
@@ -53,102 +45,144 @@ class Point:
 
 
 class ImageMatrix:
-    def __init__(self, width, height, base_img, unit, img_index=0):
+    def __init__(self, img_name, width, height, base_img, panels, unit=60):
+        self.img_name = img_name
         self.width = width
         self.height = height
         self.base_img = base_img
         self.last_img = base_img
         self.cur_img = base_img
+        self.next_img = base_img
+        self.panels = panels
         self.unit = unit
-        self.img_index = img_index
         self.img_duration = [[datetime.timedelta(0) for x in range(height)] for y in range(width)]
         self.click_pos = Point(0, 0)
         self.prev_time = 0
         self.cur_time = 0
-        self.init_panels()
 
-    def init_panels(self):
-        self.panel1 = tk.Label(root)
-        self.panel1.pack(side="left", fill="both", expand="yes")
-        self.panel2 = tk.Label(root)
-        self.panel2.pack(side="right", fill="both", expand="yes")
 
-        self.panel1.bind('<Button-1>', self.click)
-        self.panel1.bind('<B1-Motion>', self.move)
-        self.panel2.bind('<Button-1>', self.click)
-        self.panel2.bind('<B1-Motion>', self.move)
-        self.update_images()
-
-    def click(self, event):
-        self.click_pos = Point(event.x, event.y)
+    def click(self, click_pos):
+        self.click_pos = click_pos
         self.base_img = self.cur_img
         return
 
-    def move(self, event):
-        diff_x = self.click_pos.x - event.x
-        diff_y = self.click_pos.y - event.y
+    def move(self, move_pos):
+        diff_x = self.click_pos.x - move_pos.x
+        diff_y = self.click_pos.y - move_pos.y
 
         img_diff_x = int(round(diff_x / float(self.unit)))
         img_diff_y = int(round(diff_y / float(self.unit)))
 
-        new_img = Point(clamp(self.base_img.x + img_diff_x, 0, self.width - 1),
+        self.next_img = Point(clamp(self.base_img.x + img_diff_x, 0, self.width - 1),
                         clamp(self.base_img.y + img_diff_y, 0, self.height - 1))
 
-        if new_img != self.cur_img:
-            self.last_img = self.cur_img
-            self.cur_img = new_img
-            self.update_images()
-
-    def next_img(self, event):
-        if (self.img_index + 1)  <= (len(IMG_NAMES)-1):
-            self.img_index += 1
-            self.update_images()
-
-    def prev_img(self, event):
-        if (self.img_index - 1)  >= 0:
-            self.img_index -= 1
+        if self.next_img != self.cur_img:
             self.update_images()
 
     def update_images(self):
-        img_name = '{}_{:02}_{:02}.jpg'.format(IMG_NAMES[self.img_index], self.cur_img.x, self.cur_img.y)
+        self.close_img()
 
+        self.last_img = self.cur_img
+        self.cur_img = self.next_img
+
+        img_name = '{}_{:02}_{:02}.jpg'.format(self.img_name, self.cur_img.x, self.cur_img.y)
         new_img = ImageTk.PhotoImage(Image.open(IMG_PATH_PREFIX + img_name))
-        self.panel1.configure(image=new_img)
-        self.panel1.image = new_img
-        self.panel2.configure(image=new_img)
-        self.panel2.image = new_img
 
-        self.write_end_duration()
+        self.panels[0].configure(image=new_img)
+        self.panels[0].image = new_img
+        self.panels[1].configure(image=new_img)
+        self.panels[1].image = new_img
+
 
         f.write("Displaying '{}'  start: {}  ".format(img_name, self.cur_time.strftime('%H:%M:%S.%f')))
 
-    def write_end_duration(self):
+    def close_img(self):
         self.prev_time = self.cur_time
         self.cur_time = datetime.datetime.now()
 
         if self.prev_time != 0:
             duration = self.cur_time - self.prev_time
-            self.img_duration[self.last_img.y][self.last_img.x] += duration
-            total_duration = self.img_duration[self.last_img.y][self.last_img.x]
+            self.img_duration[self.cur_img.x][self.cur_img.y] += duration
+            total_duration = self.img_duration[self.cur_img.x][self.cur_img.y]
 
             f.write("end: {}  duration: {}  total duration: {}\n".format(self.cur_time.strftime('%H:%M:%S.%f'),
                                                                          duration,
                                                                          total_duration))
 
+
+class Slideshow:
+    def __init__(self, images, panels, base_img_index=0):
+        self.images = images
+        self.img_index = base_img_index
+        self.cur_img = images[base_img_index]
+        self.cur_img.update_images()
+
+        self.panels = panels
+
+        self.panels[0].bind('<Button-1>', self.click)
+        self.panels[0].bind('<B1-Motion>', self.move)
+        self.panels[1].bind('<Button-1>', self.click)
+        self.panels[1].bind('<B1-Motion>', self.move)
+
+
+    def next_img(self, event):
+        if (self.img_index + 1)  <= (len(self.images)-1):
+            self.cur_img.close_img()
+            self.cur_img.cur_time = 0
+            f.write("\n")
+            self.img_index += 1
+            self.cur_img = self.images[self.img_index]
+            self.cur_img.update_images()
+
+    def prev_img(self, event):
+        if (self.img_index - 1)  >= 0:
+            self.cur_img.close_img()
+            self.cur_img.cur_time = 0
+            f.write("\n")
+            self.img_index -= 1
+            self.cur_img = self.images[self.img_index]
+            self.cur_img.update_images()
+
+    def click(self, event):
+        click_pos = Point(event.x, event.y)
+        self.cur_img.click(click_pos)
+        return
+
+    def move(self, event):
+        move_pos = Point(event.x, event.y)
+        self.cur_img.move(move_pos)
+        return
+
     def close(self):
-        self.write_end_duration()
+        self.cur_img.close_img()
 
         f.flush()
         f.close()
         root.quit()
 
 
-img_matrix = ImageMatrix(3, 3, Point(1, 1), 60)
-root.focus_set()
-root.bind('<Left>', img_matrix.prev_img)
-root.bind('<Right>', img_matrix.next_img)
+root = tk.Tk()
+root.tk.call('tk', 'scaling', 2.0)
+root.title("lf-tracking")
+root.configure(background=BG_COLOR)
 
-root.protocol("WM_DELETE_WINDOW", img_matrix.close)
+panel1 = tk.Label(root)
+panel1.pack(side="left", fill="both", expand="yes")
+panel2 = tk.Label(root)
+panel2.pack(side="right", fill="both", expand="yes")
+panels = [panel1, panel2]
+
+img0 = ImageMatrix("img0", 3, 3, Point(1, 1), panels)
+img1 = ImageMatrix("img1", 3, 3, Point(1, 1), panels)
+img2 = ImageMatrix("img2", 3, 3, Point(1, 1), panels)
+
+slideshow = Slideshow([img0, img1, img2], panels)
+
+root.focus_set()
+root.bind('<Left>', slideshow.prev_img)
+root.bind('<Right>', slideshow.next_img)
+
+root.protocol("WM_DELETE_WINDOW", slideshow.close)
 
 app = FullScreenApp(root)
 root.mainloop()
