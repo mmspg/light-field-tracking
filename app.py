9f2ab11676b42f9e3bf0dfc864724af199feb3e9
Tanguy Albrici <tanguy.albrici@epfl.ch>
2017-06-15 19:03:40 +0200

    add 3 example images

--- app.py
+++ app.py
@@ -9,23 +9,9 @@ from helper import Helper
 images = []
 
 # Add the images to be tested here
-images.append(LFImage("I01R2"))
-images.append(LFImage("I02R1"))
-images.append(LFImage("I04R1"))
-images.append(LFImage("I09R1"))
-images.append(LFImage("I10R1"))
-
-images.append(LFImage("I01R2"))
+images.append(LFImage("I01R1"))
 images.append(LFImage("I02R2"))
-images.append(LFImage("I04R2"))
-images.append(LFImage("I09R2"))
-images.append(LFImage("I10R2"))
-
-images.append(LFImage("I01R3"))
-images.append(LFImage("I02R3"))
 images.append(LFImage("I04R3"))
-images.append(LFImage("I09R3"))
-images.append(LFImage("I10R3"))
 
 question = "How would you rate the impairment of the test image compared to the reference image?"
 answers = [1, 2, 3, 4, 5]
