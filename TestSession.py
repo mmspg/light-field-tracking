74c9e2cb39abeba2d52401bff26abfc9cb06ce59
Irene Viola <ireneviola@stidhcp-2-186.epfl.ch>
2017-07-31 16:02:31 +0200

    added license file, copyright

--- TestSession.py
+++ TestSession.py
@@ -4,6 +4,28 @@ from PIL import Image, ImageTk
 from helper import BG_COLOR, NB_IMAGES_PRELOADED, IMG_PATH_PREFIX, f_tracking, f_answers, Helper
 
 
+
+#   TestSession.py
+#   lf-tracking
+#   Created by Tanguy Albrici under the supervision of Irene Viola
+#
+#  Copyright (C) 2017 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland
+#  Multimedia Signal Processing Group
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 class TestSession:
     """Represents a test session for the assessment of light field images.
 
